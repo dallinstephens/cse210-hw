731499a85dc0c257ea989a7f9074bf6a95fd73e0
DallinS <dallinstephens1@gmail.com>
2023-09-16 08:35:51 -0600

    C# Prep 2: Core Requirement 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string gradePercent = Console.ReadLine();
+        int gradePercentage = int.Parse(gradePercent);
+
+        if (gradePercentage >= 90)
+        {
+            Console.Write("Letter Grade: A");
+        }
+        else if (gradePercentage >= 80)
+        {
+            Console.Write("Letter Grade: B");
+        }
+        else if (gradePercentage >= 70)
+        {
+            Console.Write("Letter Grade: C");
+        }
+        else if (gradePercentage >= 60)
+        {
+            Console.Write("Letter Grade: D");
+        }
+        else
+        {
+            Console.Write("Letter Grade: F");
+        }
     }
 }
