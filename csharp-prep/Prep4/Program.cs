cba3ae9e5d55411c7b44aca700f2a010626b028a
DallinS <dallinstephens1@gmail.com>
2023-09-21 06:25:48 -0600

    C# Prep 4: Core Requirement 1

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,36 @@
+// Reference for this assignment: https://byui-cse.github.io/cse210-course-2023/unit01/csharp-4.html
+
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis; // Needed to create lists.
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int number = -1; // need some initial number value to enter while loop
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        // Reference of while loop: https://byui-cse.github.io/cse210-course-2023/unit01/csharp-3.html  
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+        }
+
+        int sum = 0; // sum needs to be 0 to start with
+
+        foreach (int num in numbers)
+        {
+            // example: first time num = 18 and sum = 0
+            // second time num = 36 and sum = 18
+            sum += num; // same as sum = sum + num
+        }
+
+        Console.Write($"The sum is: {sum}");
     }
 }
