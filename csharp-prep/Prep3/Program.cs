eb59196cbb6e8cdd39597ea53ea80b5f3df2d606
DallinS <dallinstephens1@gmail.com>
2023-09-20 21:46:02 -0600

    C# Prep 3: Core Requirement 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -11,21 +11,26 @@ class Program
         // Reference for int.Parse() function: https://byui-cse.github.io/cse210-course-2023/unit01/csharp-2.html
         int magicNumber = int.Parse(magicNumberString);
 
-        Console.Write("What is your guess? ");
-        string guessString = Console.ReadLine();
-        int guess = int.Parse(guessString);
+        int guess;
 
-        if (guess < magicNumber)
+        do
         {
-            Console.WriteLine("Higher");
-        }
-        else if (guess > magicNumber)
-        {
-            Console.WriteLine("Lower");
-        }
-        else
-        {
-            Console.WriteLine("You guessed it!");
-        }
+            Console.Write("What is your guess? ");
+            string guessString = Console.ReadLine();
+            guess = int.Parse(guessString);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guess != magicNumber);
     }
 }
