952be3e75c40c975cb7382491e7ebe021f3b0f5a
DallinS <dallinstephens1@gmail.com>
2023-09-20 21:39:38 -0600

    C# Prep 3: Core Requirement 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
+// Reference for this assignment: https://byui-cse.github.io/cse210-course-2023/unit01/csharp-3.html
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string magicNumberString = Console.ReadLine();
+        // Reference for int.Parse() function: https://byui-cse.github.io/cse210-course-2023/unit01/csharp-2.html
+        int magicNumber = int.Parse(magicNumberString);
+
+        Console.Write("What is your guess? ");
+        string guessString = Console.ReadLine();
+        int guess = int.Parse(guessString);
+
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
