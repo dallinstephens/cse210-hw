4d707c9b3f7a50c050350d2b2a2ac8f3f7b04415
DallinS <dallinstephens1@gmail.com>
2023-09-27 06:46:45 -0600

    C# Learning02 Steps 1-4: Design the Classes, Start the Project, Create the JobClass, Test your Job Class

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,29 @@
+// Reference for assignment: https://byui-cse.github.io/cse210-course-2023/unit02/prepare.html
+
+public class Job
+{
+    // Attributes: member variables
+    // CLASS DIAGRAM ATTRIBUTES
+    // Job
+    // _company : string
+    // _jobTitle : string
+    // _startYear : int
+    // _endYear : int 
+
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    // Method(s): member function(s)
+    // CLASS DIAGRAM METHOD(S)
+    // Job
+    // Display() : void
+
+    public void Display()
+    {
+        // Format: "Job Title (Company) StartYear-EndYear"
+        // Example: Software Engineer (Microsoft) 2019-2022
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        Console.WriteLine(job1._company);
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job1._jobTitle = "Manager";
+        Console.WriteLine(job2._company);
     }
 }
