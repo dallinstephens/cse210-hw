9fa13fa76d63b0031057cfbaec82ed91018ddcb1
DallinS <dallinstephens1@gmail.com>
2023-10-09 03:10:04 -0600

    Encapulation Learning Activity Step 5: Create the Getters and Setters - doing this another way

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -7,28 +7,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Fraction fraction1 = new Fraction();
-        Console.WriteLine(fraction1.GetTop());
-        Console.WriteLine(fraction1.GetBottom());
-        fraction1.SetTop(8);
-        fraction1.SetBottom(9);
-        Console.WriteLine(fraction1.GetTop());
-        Console.WriteLine(fraction1.GetBottom());
+        Fraction fraction = new Fraction();
+        Console.WriteLine(fraction.GetTop());
+        Console.WriteLine(fraction.GetBottom());
+        fraction.SetTop(8);
+        fraction.SetBottom(9);
+        Console.WriteLine(fraction.GetTop());
+        Console.WriteLine(fraction.GetBottom());
 
-        Fraction fraction2 = new Fraction(6);
-        Console.WriteLine(fraction2.GetTop());
-        Console.WriteLine(fraction2.GetBottom());
-        fraction2.SetTop(10);
-        fraction2.SetBottom(11);
-        Console.WriteLine(fraction2.GetTop());
-        Console.WriteLine(fraction2.GetBottom());
+        fraction = new Fraction(6);
+        Console.WriteLine(fraction.GetTop());
+        Console.WriteLine(fraction.GetBottom());
+        fraction.SetTop(10);
+        fraction.SetBottom(11);
+        Console.WriteLine(fraction.GetTop());
+        Console.WriteLine(fraction.GetBottom());
 
-        Fraction fraction3 = new Fraction(6, 7);
-        Console.WriteLine(fraction3.GetTop());
-        Console.WriteLine(fraction3.GetBottom());
-        fraction3.SetTop(12);
-        fraction3.SetBottom(13);
-        Console.WriteLine(fraction3.GetTop());
-        Console.WriteLine(fraction3.GetBottom());
+        fraction = new Fraction(6, 7);
+        Console.WriteLine(fraction.GetTop());
+        Console.WriteLine(fraction.GetBottom());
+        fraction.SetTop(12);
+        fraction.SetBottom(13);
+        Console.WriteLine(fraction.GetTop());
+        Console.WriteLine(fraction.GetBottom());
     }
 }
