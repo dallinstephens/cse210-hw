7b12edc8e3bb40ce32034202a15f5be1f02e33e1
DallinS <dallinstephens1@gmail.com>
2023-11-07 20:35:27 -0700

    Polymorphism Learning05 Shape Assignment

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,21 @@
+using System.Reflection.Metadata.Ecma335;
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius)
+        : base(color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        // Reference for pi:
+        // http://www.java2s.com/Tutorials/CSharp/System/Math/C_Math_PI.htm
+        // Reference for exponent:
+        // https://www.tutorialspoint.com/math-pow-method-in-chash
+        return Math.PI * Math.Pow(_radius, 2);
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,41 @@
+// Reference for assignment:
+// https://byui-cse.github.io/cse210-course-2023/unit05/prepare.html
+// Reference for sample solution:
+// https://github.com/byui-cse/cse210-student-sample-solutions/blob/main/prepare/Learning05/Shape.cs
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Green", 5);
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
+
+        Console.WriteLine();
+
+        Rectangle rectangle = new Rectangle("Red", 6, 4);
+        Console.WriteLine(rectangle.GetColor());
+        Console.WriteLine(rectangle.GetArea());
+
+        Console.WriteLine();
+
+        Circle circle = new Circle("Yellow", 8);
+        Console.WriteLine(circle.GetColor());
+        Console.WriteLine(circle.GetArea());
+
+        Console.WriteLine();
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
+
     }
 }
