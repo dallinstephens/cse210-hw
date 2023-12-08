5c4d9df18402ae8e7e5693f866c6d2dd27f33d69
DallinS <dallinstephens1@gmail.com>
2023-12-07 19:31:14 -0700

    Final Project Foundation2

--- final/Foundation2/Product.cs
+++ final/Foundation2/Product.cs
@@ -3,11 +3,13 @@ public class Product
     private string _name;
     private string _id;
     private double _price;
+    private int _quantity;
 
     public Product(string name, string id, double price, int quantity)
     {
         _name = name;
         _id = id;
+        _quantity = quantity;
         _price = price * quantity;
     }
 
@@ -18,6 +20,6 @@ public class Product
 
     public override string ToString()
     {
-        return $"Name: {_name}, Product ID: {_id}\n";
+        return $"Name: {_name}, Product ID: {_id}, Quantity: {_quantity}\n";
     }
 }
